77c3b74261831edd08784bf3c76facc86bb5d54a
SynolaAlbuquerque <synola411@gmail.com>
2025-10-23 19:05:57 +0530

    Update Gold_data_scraper.py

--- Gold_data_scraper.py
+++ Gold_data_scraper.py
@@ -4,4 +4,4 @@ data = yf.download("GC=F", period="6mo", interval="1d")
 print(data.head())
 data.to_csv("gold_data.csv")
 from google.colab import files
-files.download("gold_stock_data.csv")
+files.download("gold_data.csv")
