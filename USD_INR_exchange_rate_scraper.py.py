ebb9c8f60aa03a216ff30354b791bea352c955ee
SynolaAlbuquerque <synola411@gmail.com>
2025-10-27 11:49:14 +0530

    Update USD_INR_exchange_rate_scraper.py.py

--- USD_INR_exchange_rate_scraper.py.py
+++ USD_INR_exchange_rate_scraper.py.py
@@ -1,7 +1,8 @@
-pip install yfinance
+!pip install yfinance --quiet
 import yfinance as yf
-data = yf.download("GC=F", period="6mo", interval="1d")
+from google.colab import files
+data = yf.download("INR=X", start="2021-01-04", end="2025-10-27", interval="1d")
 print(data.head())
 data.to_csv("USD_INR_data.csv")
-from google.colab import files
 files.download("USD_INR_data.csv")
+
